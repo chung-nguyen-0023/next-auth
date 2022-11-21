cbee46dcd1bbc1ec42c12e5bb4ccfc1b3f311a49
Chung Nguyen <chung.nguyen1@tda.company>
2022-11-21 13:56:31 +0700

    login with google

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Layout from "../layouts/Layout";
+import { getSession } from "next-auth/react";
 
 export default function Home() {
   const [message, setMessage] = useState("");
@@ -8,25 +9,30 @@ export default function Home() {
 
   useEffect(() => {
     (async () => {
-      const token = localStorage.getItem("jwt_token_key");
+      const session = await getSession();
+      if (session) {
+        setMessage(`Hi ${session.user.name}`);
+      } else {
+        const token = localStorage.getItem("jwt_token_key");
 
-      try {
-        const response = await fetch("http://localhost:8000/api/me", {
-          headers: { Authorization: "Bearer " + token },
-        });
-        if (response.status !== 200) {
+        try {
+          const response = await fetch("http://localhost:8000/api/me", {
+            headers: { Authorization: "Bearer " + token },
+          });
+          if (response.status !== 200) {
+            localStorage.removeItem("jwt_token_key");
+            await router.push("/login");
+          } else {
+            const content = await response.json();
+            setMessage(`Hi ${content.data.name}`);
+          }
+        } catch (e) {
           localStorage.removeItem("jwt_token_key");
           await router.push("/login");
-        } else {
-          const content = await response.json();
-          setMessage(`Hi ${content.data.name}`);
         }
-      } catch (e) {
-        localStorage.removeItem("jwt_token_key");
-        await router.push("/login");
       }
     })();
-  });
+  }, [router]);
 
   return <Layout>{message}</Layout>;
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,21 @@
 import { useRouter } from "next/router";
-import React, { SyntheticEvent, useState } from "react";
+import React, { SyntheticEvent, useState, useEffect } from "react";
 import Layout from "../layouts/Layout";
+import { getSession, signIn } from "next-auth/react";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      const session = await getSession();
+      if (session) {
+        router.push("/");
+      }
+    })();
+  }, [router]);
 
   const submit = async (e: SyntheticEvent) => {
     e.preventDefault();
@@ -27,32 +37,37 @@ const Login = () => {
 
   return (
     <Layout>
-      <form onSubmit={submit}>
-        <h1 className="h3 mb-3 fw-normal">Đăng nhập</h1>
+      <div className="login-page-wrapper">
+        <form onSubmit={submit}>
+          <h1 className="h3 mb-3 fw-normal">Đăng nhập</h1>
 
-        <div className="form-floating">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-floating">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Mật khẩu"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Mật khẩu"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Đăng nhập
-        </button>
-      </form>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Đăng nhập
+          </button>
+        </form>
+        <div className="social-login-wrapper">
+          <button className="google" onClick={() => signIn("google")}>Đăng nhập bằng Google</button>
+        </div>
+      </div>
     </Layout>
   );
 };
